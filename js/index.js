e1ae2fcfa474b3c298fc1900eeb2be16f1b65e6f
ivanpig <ivan.pig@foxmail.com>
2014-11-29 15:11:56 +0800

    search

--- js/index.js
+++ js/index.js
@@ -1,52 +1,64 @@
 $(function(){
-    //主题星期变换颜色开始
-    var dayweek=new Date().getDay();
-    switch (dayweek) {
-        case 0:
-            $('.sidebar,.search-query').css({  "background": "#4b4b49" });
-            break;
-        case 1:
-            $('.sidebar,.search-query').css({  "background": "#1c5461" });
-            break;
-        case 2:
-            $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
-            break;
-        case 3:
-            $('.sidebar,.search-query').css({  "background": "#273e38" });
-            break;
-        case 4:
-            $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
-            //"color": "#ff0011",parseInt(6*Math.random())
-            break;
-        case 5:
-            $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
-            break;
-        case 6:
-            $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
-            break;
+  //主题星期变换颜色开始
+  var dayweek=new Date().getDay();
+  switch (dayweek) {
+    case 0:
+      $('.sidebar,.search-query').css({  "background": "#4b4b49" });
+      break;
+    case 1:
+      $('.sidebar,.search-query').css({  "background": "#1c5461" });
+      break;
+    case 2:
+      $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
+      break;
+    case 3:
+      $('.sidebar,.search-query').css({  "background": "#273e38" });
+      break;
+    case 4:
+      $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
+      //"color": "#ff0011",parseInt(6*Math.random())
+      break;
+    case 5:
+      $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
+      break;
+    case 6:
+      $('.sidebar,.search-query').css({  "background": "#3D4F5D" });
+      break;
+  }
+  //主题星期变换颜色结束
+  NProgress.start();
+  setTimeout(function () {
+    NProgress.done();
+
+  }, 500);
+  var tags_a = $(".post-category");
+  tags_a.each(function(){
+    var x = 4;
+    var y = 0;
+    var rand = parseInt(Math.random() * (x - y + 1) + y);
+    $(this).addClass("post-category-"+rand);
+  });
+  $(window).scroll(function() {
+    if($(window).scrollTop() >= 100){
+      $('.topfade').fadeIn(300);
+    }else{
+      $('.topfade').fadeOut(300);
     }
-    //主题星期变换颜色结束
-    NProgress.start();
-    setTimeout(function () {
-        NProgress.done();
+  });
 
-    }, 500);
-    var tags_a = $(".post-category");
-    tags_a.each(function(){
-        var x = 4;
-        var y = 0;
-        var rand = parseInt(Math.random() * (x - y + 1) + y);
-        $(this).addClass("post-category-"+rand);
-    });
-    $(window).scroll(function() {
-        if($(window).scrollTop() >= 100){
-            $('.topfade').fadeIn(300);
-        }else{
-            $('.topfade').fadeOut(300);
-        }
-    });
+  $('.topfade').click(function(){
+    $('html,body').animate({scrollTop: '0px'}, 800);});
 
-    $('.topfade').click(function(){
-        $('html,body').animate({scrollTop: '0px'}, 800);});
-
+//   $("#query").keydown(function(e){
+//     if(e.keyCode == 13){
+//       var v = $(this).val();
+//       if(v == "")return false;
+//       $.ajax({
+//         dataType: 'jsonp',
+//         url: "http://www.tapirgo.com/api/1/search.json?token=547969cf34932d8eec000002&query="+v,
+//         success: function(data){
+//            alert(data.summary);
+//         }});
+//     }
+//   });
 });
